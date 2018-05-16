430731dc2120a0cae4eaf0a3204cc438ab755fb3
leoomedeiros <leomedeiros17@gmail.com>
2018-05-16 09:09:56 -0300

    Adicionando e mostrando apenas os pendentes (Falta criar uma tela para os finalizados e uma função para trocar o boolean do actve)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,41 @@
+
+
+
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import { ContactProvider, Contact, ContactList } from '../../providers/contact/contact';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  contacts: ContactList[];
+  constructor(public navCtrl: NavController,private contactProvider: ContactProvider, private toast: ToastController) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  ionViewDidEnter(){
+   this.contactProvider.getAll()
+    .then(results =>{
+      this.contacts = results;
+    })
+  }
+  addContact(){
+    this.navCtrl.push('EditContactPage')
+  }
+  editContact(item: ContactList){
+    this.navCtrl.push('EditContactPage', {key: item.key, contact:item.contact});
+  }
+  removeContact(item: ContactList){
+    this.contactProvider.remove(item.key)
+      .then(()=>{
+        let index = this.contacts.indexOf(item);
+        this.contacts.splice(index,1);
+
+        this.toast.create({message: "Removido com sucesso!", duration: 3000, position: "bottom"}).present();
+      })
   }
 
 }
